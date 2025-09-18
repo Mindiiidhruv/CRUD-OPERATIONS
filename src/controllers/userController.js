452df0942c905e02d36a319a261affe6cc3d40c9
Mindiiidhruv <mindiiidhruv@gmail.com>
2025-09-18 17:58:57 +0530

    updated pagination module

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -110,7 +110,6 @@ exports.PaginationList = async (req, res) => {
   try {
     const page = parseInt(req.query.page) || 1;
     let limit = parseInt(req.query.limit) || 10;
-    limit = limit > 50 ? 50 : limit;
     const skip = (page - 1) * limit;
 
     const users = await User.find().skip(skip).limit(limit);
