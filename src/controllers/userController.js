4c79cecb7e991e033ddfacae465bcdeea34003b3
Mindiiidhruv <mindiiidhruv@gmail.com>
2025-09-19 16:44:30 +0530

    Added a response message to create user api

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -3,7 +3,7 @@ const User = require("../models/Users");
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
-    res.status(201).json(user);
+    res.status(200).json({ message: "User created successfully", user });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -35,7 +35,7 @@ exports.getUsers = async (req, res) => {
     const total = await User.countDocuments(query);
 
     if (!users.length) {
-      return res.status(404).json({ error: "No users found" });
+      return res.status(404).json({ error: "User not found" });
     }
 
     res.status(200).json({
